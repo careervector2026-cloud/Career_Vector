656ab79b00b9c3fa23ff4d81d210ced0f9289f61
Akshaykumar110904 <ravulakariakshaykumar@gmail.com>
2026-01-30 14:35:35 +0530

    modified CareerVectorApplication file

--- src/main/java/com/careervector/CareerVectorApplication.java
+++ src/main/java/com/careervector/CareerVectorApplication.java
@@ -2,12 +2,25 @@ package com.careervector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class CareerVectorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CareerVectorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(CareerVectorApplication.class, args);
 
+        // --- ADD THIS DEBUGGING BLOCK ---
+        Environment env = context.getEnvironment();
+        String url = env.getProperty("spring.datasource.url");
+        String user = env.getProperty("spring.datasource.username");
+
+        System.out.println("==========================================");
+        System.out.println("DEBUG: RENDER DB CONNECTION INFO");
+        System.out.println("URL: " + url);
+        System.out.println("User: " + user);
+        System.out.println("==========================================");
+        // --------------------------------
+    }
 }
