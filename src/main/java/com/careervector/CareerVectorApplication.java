9a72800a7013619e88049493abacb1e4968b5c1a
Akshaykumar110904 <ravulakariakshaykumar@gmail.com>
2026-01-31 10:54:31 +0530

    modified CareerVectorApplication and application.properties files

--- src/main/java/com/careervector/CareerVectorApplication.java
+++ src/main/java/com/careervector/CareerVectorApplication.java
@@ -7,19 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CareerVectorApplication {
 
     public static void main(String[] args) {
-        // --- DEBUG BLOCK: RUNS BEFORE APP STARTS ---
-        System.out.println("========================================");
-        System.out.println("DEBUG: CHECKING RENDER VARIABLES...");
-
-        // Read the raw environment variable from Render
-        String dbUrl = System.getenv("DB_URL");
-        String dbUser = System.getenv("DB_USERNAME");
-
-        System.out.println("RAW DB_URL: [" + dbUrl + "]");
-        System.out.println("RAW DB_USER: [" + dbUser + "]");
-        System.out.println("========================================");
-        // -------------------------------------------
-
         SpringApplication.run(CareerVectorApplication.class, args);
     }
+
 }
